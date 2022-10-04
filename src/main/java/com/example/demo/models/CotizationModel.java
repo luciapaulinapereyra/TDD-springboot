0291eed4b60dabe9a82ab2eb21d59d47cf2f589d
Lucía <lumes77iza@gmail.com>
2022-10-04 15:19:11 -0300

    adding tests in red!

--- src/main/java/com/example/demo/models/CotizationModel.java
+++ src/main/java/com/example/demo/models/CotizationModel.java
@@ -11,6 +11,14 @@ import javax.persistence.Table;
 public class CotizationModel {
 	private Date checkDate;
 	private Date paymentDay;
+	private Integer operationTerm;
+	private Double checkAmount;
+	private Double dailyRate;
+	private Double interests;
+	private Double expenseRate;
+	private Double managmentExpense;
+	private Double iva;
+	private Double netOperation;
 
 	public Calendar calculatePaymentDate(Calendar checkDate) {
 		checkDate.add(Calendar.DAY_OF_YEAR, 2);
@@ -34,5 +42,86 @@ public class CotizationModel {
 	public void setPaymentDay(Date paymentDay) {
 		this.paymentDay = paymentDay;
 	}
+
+	public Integer getOperationTerm() {
+		return operationTerm;
+	}
+
+	public void setOperationTerm(Integer operationTerm) {
+		this.operationTerm = operationTerm;
+	}
+
+	public void calculateOperationTerm() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Double getCheckAmount() {
+		return checkAmount;
+	}
+
+	public void setCheckAmount(Double checkAmount) {
+		this.checkAmount = checkAmount;
+	}
+
+	public Double getDailyRate() {
+		return dailyRate;
+	}
+
+	public void setDailyRate(Double dailyRate) {
+		this.dailyRate = dailyRate;
+	}
+
+	public Double getInterests() {
+		return interests;
+	}
+
+	public void setInterests(Double interests) {
+		this.interests = interests;
+	}
+
+	public void calculateInterests() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Double getExpenseRate() {
+		return expenseRate;
+	}
+
+	public void setExpenseRate(Double expenseRate) {
+		this.expenseRate = expenseRate;
+	}
+
+	public Double getManagmentExpense() {
+		return managmentExpense;
+	}
+
+	public void setManagmentExpense(Double managmentExpense) {
+		this.managmentExpense = managmentExpense;
+	}
+
+	public void calculateExpenseManagement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Double getIva() {
+		return iva;
+	}
+
+	public void setIva(Double iva) {
+		this.iva = iva;
+	}
+
+	public Double getNetOperation() {
+		return netOperation;
+	}
+
+	public void setNetOperation(Double netOperation) {
+		this.netOperation = netOperation;
+	}
+
+
 
 }
--- src/test/java/com/example/demo/models/CotizationModelTest.java
+++ src/test/java/com/example/demo/models/CotizationModelTest.java
@@ -13,26 +13,71 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CotizationModelTest {
+	CotizationModel cot = new CotizationModel();
+	Calendar calendar = Calendar.getInstance();
+	DateFormat formatt = new SimpleDateFormat("dd/M/yy");
+
 	@BeforeEach
 	void setUp() throws Exception {
-
+		Date checkDate = formatt.parse("11/10/2022");
+		calendar.setTime(checkDate);
+		cot.calculatePaymentDate(calendar);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		cot = null;
 	}
 
 	@Test
 	void testGetPaymentDate() throws ParseException {
-		CotizationModel cot = new CotizationModel();
 		Calendar calendar = Calendar.getInstance();
-		DateFormat formatt = new SimpleDateFormat("dd/M/yy");
-		Date checkDate = formatt.parse("11/11/2022");
-		Date expectedDate = formatt.parse("13/11/2022");
-		calendar.setTime(checkDate);
-		cot.calculatePaymentDate(calendar);
+		Date expectedDate = formatt.parse("13/10/2022");
 		calendar.setTime(expectedDate);
 		assertEquals(calendar.getTime(), cot.getPaymentDay());
 	}
 
+	@Test
+	void testGetOperationTerm() {
+		int expected = 9;
+		cot.calculateOperationTerm();
+		assertEquals(expected, cot.getOperationTerm());
+	}
+
+	@Test
+	void testGetInteres() {
+		cot.setOperationTerm(10);
+		cot.setCheckAmount(2000.0);
+		cot.setDailyRate(2.5);
+		cot.calculateInterests();
+		Double expected = 500.0;
+		assertEquals(expected, cot.getInterests());
+	}
+
+	@Test
+	void testGetExpenseManagement() {
+		cot.setCheckAmount(50000.0);
+		cot.setExpenseRate(3.5);
+		cot.calculateExpenseManagement();
+		Double expected = 1750.0;
+		assertEquals(expected, cot.getManagmentExpense());
+	}
+
+	@Test
+	void testGetIva() {
+		cot.setInterests(700.0);
+		cot.setManagmentExpense(25000.0);
+		Double expected = 5460.0;
+		assertEquals(expected, cot.getIva());
+	}
+
+	@Test
+	void testGetNetOperation() {
+		cot.setCheckAmount(10000.0);
+		cot.setInterests(1000.0);
+		cot.setManagmentExpense(250.0);
+		cot.setIva(500.0);
+		Double expected = 8250.0;
+		assertEquals(expected, cot.getNetOperation());
+	}
 }
